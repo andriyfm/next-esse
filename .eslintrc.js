3b0633a44eb7620fe6ed71ba231e9639895e7a2a
andriyfm <andrifmsyh@gmail.com>
2021-10-31 13:17:27 +0700

    Use SWC for new feture of next.js 12 and fixing eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "google", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "google",
+    "prettier",
+    "plugin:@next/next/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
